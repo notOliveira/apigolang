c8c2e5417a798b9438b24a73b3b57baa50c3b116
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2025-08-27 20:21:52 -0300

    feat: Intializing config package and creating custom logger

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"github.com/notOliveira/apigolang/config"
 	"github.com/notOliveira/apigolang/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	// Initialize the router
 	router.Initialize()
 }
