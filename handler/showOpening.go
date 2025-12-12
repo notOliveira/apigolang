a366f37f3ce2ab7e764d3cf3023dabb6c074408a
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2025-12-12 19:29:17 -0300

    feat: cleaning show opening func

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -2,24 +2,20 @@ package handler
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/notOliveira/apigolang/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	openings := []schemas.Opening{}
-
 	id := ctx.Query("id")
-
 	if id == "" {
 		sendError(ctx, http.StatusBadRequest, "Query parameter 'id' is required")
 		return
 	}
-
+	openings := []schemas.Opening{}
 	if err := db.First(&openings, id).Error; err != nil {
 		logger.Errorf("Error fetching Opening: %v", err.Error())
-		sendError(ctx, http.StatusInternalServerError, "Error fetching opening")
+		sendError(ctx, http.StatusNotFound, "Opening not found")
 		return
 	}
 	sendSuccess(ctx, "show-opening", openings)
